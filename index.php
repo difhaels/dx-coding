4c0fe9075677d1272aae706e51085cb0ceb4ac35
Agung Saputra <agungsaputradifh@gmail.com>
2024-05-10 21:54:50 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php  require 'functions.php'; $sales_order = query("SELECT * FROM sales_order"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Sales Order - Agung Saputra</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <h1>Daftar Pesanan</h1>
+    <br>
+    <a class="add" href="tambah.php">Tambah Pemesanan</a>
+    <br><br>
+
+    <br>
+
+    <table class="table" cellpadding="10" cellspacing="0" border="1">
+
+    <tr>
+        <th>No.</th>
+        <th>Kode</th>
+        <th>Nama Pemesan</th>
+        <th>Nama Kelas</th>
+    </tr>
+
+    <?php $i=1; ?>
+    <?php foreach ($sales_order as $row) : ?>
+    
+    <tr>
+
+        <td> <?php echo $i; ?></td>
+        <td> <?php echo $row['kode']; ?> </td>
+        <td> <?php echo $row['nama_pemesan']; ?> </td>
+        <td> <?php echo $row['nama_kelas']; ?> </td>
+    </tr>
+    
+    <?php $i++ ?>
+    <?php endforeach; ?>
+
+    </table>
+
+    <br>
+    <br>
+    <a class="add" href="/tugas-5/inventory.php">Lihat Inventory</a>
+    
+    
+</body>
+</html>
