84bb1f17f225d5b41aa4200b5ced5a1898b45d88
Agung Saputra <agungsaputradifh@gmail.com>
2024-05-17 22:09:19 +0700

    new home

---
+++ data_customer/data_customer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Project ERP - Data Customer</title>
+    <!-- Bootstrap CSS -->
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <nav class="bg-slate-200">
+        <h1 class="text-2xl py-3 px-3">Project ERP</h1>
+    </nav>
+    <a href="../index.php">back</a>
+</body>
+</html>
