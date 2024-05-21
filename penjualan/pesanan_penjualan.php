0ee304f31013bb93bd7783a844fb92e079b904de
Agung Saputra <agungsaputradifh@gmail.com>
2024-05-21 22:10:50 +0700

    change penjualan

--- penjualan/pesanan_penjualan.php
+++ penjualan/pesanan_penjualan.php
@@ -1,4 +1,45 @@
-<?php  require '../functions.php'; $penjualan = query("SELECT * FROM penjualan"); ?>
+<?php  
+    // Koneksi ke database
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "project_erp"; // Ganti dengan nama database Anda
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    // Periksa koneksi
+    if (!$conn) {
+        die("Koneksi gagal: " . mysqli_connect_error());
+    }
+
+    // Query untuk mengambil data penjualan dengan nama pelanggan dan nama kelas
+    $query = "
+        SELECT 
+            penjualan.id_penjualan, 
+            pelanggan.nama_pelanggan, 
+            kelas.nama_kelas, 
+            penjualan.tanggal_pembelian, 
+            penjualan.harga
+        FROM 
+            penjualan
+        JOIN 
+            pelanggan ON penjualan.id_pelanggan = pelanggan.id_pelanggan
+        JOIN 
+            kelas ON penjualan.id_kelas = kelas.id_kelas
+    ";
+
+    $result = mysqli_query($conn, $query);
+
+    // Memeriksa apakah query berhasil
+    if (!$result) {
+        die("Query gagal: " . mysqli_error($conn));
+    }
+
+    // Mengambil semua hasil dalam bentuk array asosiatif
+    $penjualan = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    mysqli_close($conn);
+?>
 
 
 <!DOCTYPE html>
@@ -30,8 +71,8 @@
             <div class="px-5 flex justify-between bg-blue-100">
                 <span class="w-[20%] py-2"> <?php echo $i; ?></span>
                 <span class="w-[20%] py-2"> <?php echo $row['id_penjualan']; ?> </span>
-                <span class="w-[20%] py-2"> <?php echo $row['id_pelanggan']; ?> </span>
-                <span class="w-[20%] py-2"> <?php echo $row['id_kelas']; ?> </span>
+                <span class="w-[20%] py-2"> <?php echo $row['nama_pelanggan']; ?> </span>
+                <span class="w-[20%] py-2"> <?php echo $row['nama_kelas']; ?> </span>
                 <span class="w-[10%] py-2"> 
                     <a href="" class="px-2 py-1 bg-red-500 text-white rounded-lg hover:bg-red-300">hapus</a>
                 </span>
