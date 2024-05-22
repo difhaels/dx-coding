a4dec359a0091fdf9cf23c8e287aa1d4fd84cbaf
Agung Saputra <agungsaputradifh@gmail.com>
2024-05-22 08:46:15 +0700

    added tambah pesanan php

--- pelanggan/data_pelanggan.php
+++ pelanggan/data_pelanggan.php
@@ -19,7 +19,6 @@
         <div class="px-5 flex justify-between bg-blue-400 text-white">
             <span class="w-[20%] py-2">No.</span>
             <span class="w-[20%] py-2">Nama Pelanggan</span>
-            <span class="w-[20%] py-2">Kelas Diikuti</span>
             <span class="w-[10%] py-2"></span>
         </div>
 
@@ -29,9 +28,8 @@
             <div class="px-5 flex justify-between bg-blue-100">
                 <span class="w-[20%] py-2"> <?php echo $i; ?></span>
                 <span class="w-[20%] py-2"> <?php echo $row['nama_pelanggan']; ?> </span>
-                <span class="w-[20%] py-2"> <?php echo $row['kelas_diikuti']; ?> </span>
                 <span class="w-[10%] py-2"> 
-                    <a href="" class="px-2 py-1 bg-red-500 text-white rounded-lg hover:bg-red-300">hapus</a>
+                    <a href="" class="px-2 py-1 bg-gray-500 text-white rounded-lg hover:bg-gray-300">edit</a>
                 </span>
             </div>
             
@@ -40,5 +38,20 @@
 
     </div>
     
+
+    <div class="m-3 mt-10 w-[30%]">
+        <h1 class="text-2xl font-semibold">Tambah Pelanggan</h1>
+        <form action="../functions/tambah_pelanggan.php" method="POST">
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="nama_pelanggan">
+                    Nama Pelanggan
+                </label>
+                <input type="text" name="nama_pelanggan" id="nama_pelanggan" placeholder="Nama Pelanggan" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300" required>
+            </div>
+            <div class="w-fit mt-3 bg-blue-400 text-white px-3 py-1 rounded-lg hover:bg-blue-300">
+                <button type="submit" class="px-2 py-1">Submit</button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
