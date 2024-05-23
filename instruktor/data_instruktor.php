36c80fb41b809224f72d473bfc6650ee149a5736
Agung Saputra <agungsaputradifh@gmail.com>
2024-05-23 11:43:22 +0700

    added tambah instruktor

--- instruktor/data_instruktor.php
+++ instruktor/data_instruktor.php
@@ -42,5 +42,32 @@
 
     </div>
     
+    <div class="m-3 mt-10 w-[30%]">
+    <h1 class="text-2xl font-semibold">Tambah Instruktur</h1>
+    <form action="../functions/tambah_instruktor.php" method="POST">
+        <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="nama_instruktor">
+                Nama Instruktur
+            </label>
+            <input type="text" name="nama_instruktor" id="nama_instruktor" placeholder="Nama Instruktur" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300" required>
+        </div>
+        <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="jumlah_kelas">
+                Jumlah Kelas
+            </label>
+            <input type="number" name="jumlah_kelas" id="jumlah_kelas" placeholder="Jumlah Kelas" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300" required>
+        </div>
+        <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="total_pelanggan">
+                Total Pelanggan
+            </label>
+            <input type="number" name="total_pelanggan" id="total_pelanggan" placeholder="Total Pelanggan" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300" required>
+        </div>
+        <div class="w-fit mt-3 bg-blue-400 text-white px-3 py-1 rounded-lg hover:bg-blue-300">
+            <button type="submit" class="px-2 py-1">Submit</button>
+        </div>
+    </form>
+</div>
+
 </body>
 </html>
