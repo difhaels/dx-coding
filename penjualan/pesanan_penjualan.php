f6d1914f98013d792b6b75e3de185cc139f09a0c
Agung Saputra <agungsaputradifh@gmail.com>
2024-06-04 21:35:29 +0700

    added tanggal penjualan

--- penjualan/pesanan_penjualan.php
+++ penjualan/pesanan_penjualan.php
@@ -51,39 +51,39 @@
 <body>
     <nav class="bg-slate-200 flex items-center justify-between py-3 px-3">
         <h1 class="text-2xl">Project ERP</h1>
-        <a  class="bg-blue-400 text-white px-3 py-1 rounded-lg hover:bg-blue-300" href="./penjualan.php">back</a>
+        <a class="bg-blue-400 text-white px-3 py-1 rounded-lg hover:bg-blue-300" href="./penjualan.php">back</a>
     </nav>
 
     <h1 class="text-xl font-semibold px-5 py-3">Pesanan Penjualan</h1>
     <div class="mx-3">
-
         <div class="px-5 flex justify-between bg-blue-400 text-white">
-            <span class="w-[20%] py-2">No.</span>
-            <span class="w-[20%] py-2">Id Penjualan</span>
-            <span class="w-[20%] py-2">Pelanggan</span>
-            <span class="w-[20%] py-2">Kelas</span>
+            <span class="w-[15%] py-2">No.</span>
+            <span class="w-[15%] py-2">Id Penjualan</span>
+            <span class="w-[15%] py-2">Pelanggan</span>
+            <span class="w-[15%] py-2">Kelas</span>
+            <span class="w-[15%] py-2">Tanggal Pembelian</span>
+            <span class="w-[15%] py-2">Harga</span>
             <span class="w-[10%] py-2"></span>
         </div>
 
-        <?php $i=1; ?>
-            <?php foreach ($penjualan as $row) : ?>
-            
+        <?php $i = 1; ?>
+        <?php foreach ($penjualan as $row) : ?>
             <div class="px-5 flex justify-between bg-blue-100">
-                <span class="w-[20%] py-2"> <?php echo $i; ?></span>
-                <span class="w-[20%] py-2"> <?php echo $row['id_penjualan']; ?> </span>
-                <span class="w-[20%] py-2"> <?php echo $row['nama_pelanggan']; ?> </span>
-                <span class="w-[20%] py-2"> <?php echo $row['nama_kelas']; ?> </span>
+                <span class="w-[15%] py-2"><?php echo $i; ?></span>
+                <span class="w-[15%] py-2"><?php echo $row['id_penjualan']; ?></span>
+                <span class="w-[15%] py-2"><?php echo $row['nama_pelanggan']; ?></span>
+                <span class="w-[15%] py-2"><?php echo $row['nama_kelas']; ?></span>
+                <span class="w-[15%] py-2"><?php echo $row['tanggal_pembelian']; ?></span>
+                <span class="w-[15%] py-2"><?php echo $row['harga']; ?></span>
                 <span class="w-[10%] py-2"> 
                     <a href="../functions/hapus_penjualan.php?id_penjualan=<?php echo $row['id_penjualan']; ?>" class="px-2 py-1 bg-red-500 text-white rounded-lg hover:bg-red-300">hapus</a>
                 </span>
             </div>
-            
-            <?php $i++ ?>
+            <?php $i++; ?>
         <?php endforeach; ?>
-
     </div>
-    
-    <div class=" m-3 mt-10 w-[40%]">
+
+    <div class="m-3 mt-10 w-[40%]">
         <h1 class="text-2xl font-semibold">Tambah Penjualan</h1>
         <form action="../functions/tambah_penjualan.php" method="POST">
             <div class="mb-4">
@@ -93,13 +93,7 @@
                 <select name="id_pelanggan" id="id_pelanggan" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300">
                     <?php
                     // Ambil data pelanggan dari database
-                    $servername = "localhost";
-                    $username = "root";
-                    $password = "";
-                    $dbname = "project_erp";
-
                     $conn = mysqli_connect($servername, $username, $password, $dbname);
-
                     if (!$conn) {
                         die("Koneksi gagal: " . mysqli_connect_error());
                     }
@@ -121,7 +115,6 @@
                     <?php
                     // Ambil data kelas dari database
                     $conn = mysqli_connect($servername, $username, $password, $dbname);
-
                     if (!$conn) {
                         die("Koneksi gagal: " . mysqli_connect_error());
                     }
@@ -152,6 +145,5 @@
             </div>
         </form>
     </div>
-
 </body>
 </html>
