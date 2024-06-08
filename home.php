e973c162912879688e0c863fbfe5ac6c1be28290
Agung Saputra <agungsaputradifh@gmail.com>
2024-06-08 19:00:35 +0700

    template home

--- home.php
+++ home.php
@@ -10,7 +10,35 @@
         <a class="bg-blue-400 text-white px-3 py-1 rounded-lg hover:bg-blue-300 cursor-pointer">Login</a>
     </nav>
 
-    
+    <div class="flex justify-between gap-10 px-5 mt-5">
+        <div class="w-full">
+            <h1 class="text-2xl">Most Sales</h1>
+            <div class="mt-5">
+                <div class="w-full">
+                    <div class="bg-red-500 h-24"></div>
+                    <div class="bg-slate-200 px-3 py-2">Nama Kelas</div>
+                </div>
+            </div>
+        </div>
+        <div  class="w-full">
+            <h1 class="text-2xl">All Class</h1>
+            <div class="mt-5">
+                <div class="w-full">
+                    <div class="bg-red-500 h-24"></div>
+                    <div class="bg-slate-200 px-3 py-2">Nama Kelas</div>
+                </div>
+            </div>
+        </div>
+        <div  class="w-full">
+            <h1 class="text-2xl">New</h1>
+            <div class="mt-5">
+                <div class="w-full">
+                    <div class="bg-red-500 h-24"></div>
+                    <div class="bg-slate-200 px-3 py-2">Nama Kelas</div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <footer class="fixed bottom-0 text-center bg-slate-900 text-white py-10 w-full">
         <h1>Dibuat Agung Saputra dengan <span class="font-bold">php</span> dan <span class="font-bold">tailwind</span></h1>
