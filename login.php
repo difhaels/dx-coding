5554c052686937ebb0fd3fd4c0bdd6ed2ae80dc5
Agung Saputra <agungsaputradifh@gmail.com>
2024-06-10 21:21:20 +0700

    login siswa

--- login.php
+++ login.php
@@ -9,8 +9,8 @@
 <body class="bg-gray-100">
     <div class="flex items-center justify-center min-h-screen">
         <div class="bg-white p-8 rounded-lg shadow-lg w-full max-w-sm">
-            <h2 class="text-2xl font-semibold mb-6 text-center">Login</h2>
-            <form action="process_login.php" method="POST">
+            <h2 class="text-2xl font-semibold mb-6 text-center">Login Siswa</h2>
+            <form action="" method="POST">
                 <div class="mb-4">
                     <label for="username" class="block text-gray-700 text-sm font-bold mb-2">Username</label>
                     <input type="text" name="username" id="username" class="block w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400" required>
@@ -19,20 +19,18 @@
                     <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Password</label>
                     <input type="password" name="password" id="password" class="block w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400" required>
                 </div>
-                <div class="mb-6">
-                    <label for="role" class="block text-gray-700 text-sm font-bold mb-2">Login As</label>
-                    <select name="role" id="role" class="block w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400" required>
-                        <option value="instruktur">Instruktur</option>
-                        <option value="admin">Admin</option>
-                    </select>
+                <div class="text-center flex justify-between mb-4">
+                    <a href="" class="text-sm text-blue-500 hover:underline">Lupa Password</a>
+                    <a href="" class="text-sm text-blue-500 hover:underline">Daftar</a>
                 </div>
-                <div class="flex items-center justify-between">
-                    <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-400">Login</button>
-                </div>
+                <button type="submit" class="w-full bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-400">Login</button>
+                <a href="">
+                    <h1  class="mt-2 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-400 text-center">Login Instruktor</h1>
+                </a>
+                <a href="">
+                    <h1  class="mt-2 bg-orange-500 text-white px-4 py-2 rounded-lg hover:bg-orange-400 focus:outline-none focus:ring-2 focus:ring-blue-400 text-center">Login Admin</h1>
+                </a>
             </form>
-            <div class="text-center mt-4">
-                <a href="register.php" class="text-sm text-blue-500 hover:underline">Daftar</a>
-            </div>
         </div>
     </div>
 </body>
