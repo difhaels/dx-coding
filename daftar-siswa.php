7c404aa379a2294c24fb43a50f802e7456493e63
Agung Saputra <agungsaputradifh@gmail.com>
2024-06-11 21:00:55 +0700

    added add siswa

---
+++ daftar-siswa.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Siswa - Project ERP</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-gray-100">
+    <div class="flex items-center justify-center min-h-screen">
+        <div class="bg-white p-8 rounded-lg shadow-lg w-full max-w-sm">
+            <h2 class="text-2xl font-semibold mb-6 text-center">Daftar Siswa</h2>
+            <form action="" method="POST">
+                <div class="mb-4">
+                    <label for="username_siswa" class="block text-gray-700 text-sm font-bold mb-2">Username</label>
+                    <input type="text" name="username_siswa" id="username_siswa" class="block w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400" required>
+                </div>
+                <div class="mb-4">
+                    <label for="email_siswa" class="block text-gray-700 text-sm font-bold mb-2">email</label>
+                    <input type="email" name="email_siswa" id="email_siswa" class="block w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400" required>
+                </div>
+                <div class="mb-4">
+                    <label for="nohp_siswa" class="block text-gray-700 text-sm font-bold mb-2">nohp</label>
+                    <input type="text" name="nohp_siswa" id="nohp_siswa" class="block w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400" required>
+                </div>
+                <div class="mb-4">
+                    <label for="password_siswa" class="block text-gray-700 text-sm font-bold mb-2">Password</label>
+                    <input type="password_siswa" name="password_siswa" id="password_siswa" class="block w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400" required>
+                </div>
+                <div class="mb-4">
+                    <label for="password_siswa" class="block text-gray-700 text-sm font-bold mb-2">Check Password</label>
+                    <input type="password_siswa" name="password_siswa" id="password_siswa" class="block w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400" required>
+                </div>
+                <button type="submit" class="w-full bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-400">Create Acc</button>
+                <a href="./home.php">
+                    <h1  class="mt-2 bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-blue-400 text-center">Exit</h1>
+                </a>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
