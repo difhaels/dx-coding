e6c6b7d3bd92486bc7dc4962f1de6ea1282f5e9a
Agung Saputra <agungsaputradifh@gmail.com>
2024-06-12 22:45:51 +0700

    added profile student

---
+++ profile/profile-student.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Dx Coding - Agung Saputra</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <nav class="bg-slate-200 flex justify-between items-center px-5">
+        <h1 class="text-2xl py-3">Dx Coding</h1>
+        <a class="bg-blue-400 text-white px-3 py-1 rounded-lg hover:bg-blue-300 cursor-pointer">Login</a>
+    </nav>
+
+    <footer class="fixed bottom-0 text-center bg-slate-900 text-white py-10 w-full">
+        <h1>Dibuat Agung Saputra dengan <span class="font-bold">php</span> dan <span class="font-bold">tailwind</span></h1>
+    </footer>
+</body>
+</html>
