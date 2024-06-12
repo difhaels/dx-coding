2b070f20807f031fef65a28ea87d8f001935f4c4
Agung Saputra <agungsaputradifh@gmail.com>
2024-06-12 22:48:38 +0700

    change siswa to student

--- login/login-student.php
+++ login/login-student.php
@@ -9,7 +9,7 @@
 <body class="bg-gray-100">
     <div class="flex items-center justify-center min-h-screen">
         <div class="bg-white p-8 rounded-lg shadow-lg w-full max-w-sm">
-            <h2 class="text-2xl font-semibold mb-6 text-center">Login Siswa</h2>
+            <h2 class="text-2xl font-semibold mb-6 text-center">Login Student</h2>
             <form action="" method="POST">
                 <div class="mb-4">
                     <label for="username" class="block text-gray-700 text-sm font-bold mb-2">Username</label>
@@ -20,8 +20,8 @@
                     <input type="password" name="password" id="password" class="block w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400" required>
                 </div>
                 <div class="text-center flex justify-between mb-4">
-                    <a href="" class="text-sm text-blue-500 hover:underline">Lupa Password</a>
-                    <a href="" class="text-sm text-blue-500 hover:underline">Daftar</a>
+                    <a href="" class="text-sm text-blue-500 hover:underline">Forgot Password</a>
+                    <a href="" class="text-sm text-blue-500 hover:underline">Create Account</a>
                 </div>
                 <button type="submit" class="w-full bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-400">Login</button>
                 <a href="./login-instruktor.php">
