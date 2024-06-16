fe03aebbde634d417c9e04e4fd04e66b134e1a86
Agung Saputra <agungsaputradifh@gmail.com>
2024-06-16 08:59:23 +0700

    tampilan about

---
+++ admin/about.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Project ERP - Agung Saputra</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <nav class="bg-slate-200">
+        <h1 class="text-2xl py-3 px-3">Dx Coding</h1>
+    </nav>
+
+    <div class="flex">
+        <div class="w-[10%] bg-slate-700 h-screen text-white">
+            <a href="">
+                <div class=" hover:bg-slate-600 p-2">Course</div>
+            </a>
+            <a href="">
+                <div class=" hover:bg-slate-600 p-2">Finance</div>
+            </a>
+            <a href="">
+                <div class=" hover:bg-slate-600 p-2">About</div>
+            </a>
+        </div>
+        
+        <div>
+            <h1 class="m-5 text-xl">About</h1>
+        </div>
+    </div>
+
+    <footer class="fixed bottom-0 text-center bg-slate-900 text-white py-10 w-full">
+        <h1>Dibuat Agung Saputra dengan <span class="font-bold">php</span> dan <span class="font-bold">tailwind</span></h1>
+    </footer>
+</body>
+</html>
