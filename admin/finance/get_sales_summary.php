04e165f23a19e8cfa6a71540f276e8d7ad810b26
Agung Saputra <agungsaputradifh@gmail.com>
2024-06-20 23:52:03 +0700

    fix

--- admin/finance/get_sales_summary.php
+++ admin/finance/get_sales_summary.php
@@ -4,8 +4,8 @@ require '../../functions/query.php';
 // Function to get the total sales price for a given month
 function getTotalSalesPrice($month = null) {
     $query = "SELECT SUM(c.price_course) as total_sales
-              FROM sale s
-              JOIN course c ON s.course_sale = c.id_course";
+            FROM sale s
+            JOIN course c ON s.course_sale = c.id_course";
     if ($month) {
         $query .= " WHERE MONTH(s.date_sale) = $month AND YEAR(s.date_sale) = 2024";
     }
