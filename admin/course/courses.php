8b93e009b2b48fddb8253c73a861777e430704bc
Agung Saputra <agungsaputradifh@gmail.com>
2024-06-25 08:49:50 +0700

    add option course

--- admin/course/courses.php
+++ admin/course/courses.php
@@ -43,6 +43,7 @@
                         <th class="px-4 py-2">Date</th>
                         <th class="px-4 py-2">Instructor Name</th>
                         <th class="px-4 py-2">Price (Rupiah)</th>
+                        <th class="px-4 py-2">Option</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -56,6 +57,18 @@
                             <td class="border px-4 py-2"><?= $course["date_course"]; ?></td>
                             <td class="border px-4 py-2"><?= $course["name_instructor"]; ?></td>
                             <td class="border px-4 py-2"><?= number_format($course["price_course"], 0, ',', '.'); ?></td>
+                            <td class="border px-4 py-2 grid grid-cols-1 gap-1">
+                                <a href="">
+                                    <div class="bg-slate-500 hover:bg-slate-400 shadow-lg rounded-lg px-2 py-1 text-white">
+                                        <h1 class="text-center">edit</h1>
+                                    </div>
+                                </a>
+                                <a href="">
+                                    <div class="bg-red-500 hover:bg-red-400 shadow-lg rounded-lg px-2 py-1 text-white">
+                                        <h1 class="text-center">delete</h1>
+                                    </div>
+                                </a>
+                            </td>
                         </tr>
                     <?php endforeach ?>
                 </tbody>
